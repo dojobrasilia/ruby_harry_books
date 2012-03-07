1f9d5c09476076df88b8dbf2f0b1ab7b1612fd52
Fabricio Buzeto <nuk.anime13@gmail.com>
2012-03-07 17:41:46 -0300

    Refactored the test so the class is instatiated only once.

--- harry_books_spec.rb
+++ harry_books_spec.rb
@@ -1,28 +1,32 @@
 require './harry_books'
 
 describe "HarryPotterBookShop" do
+
+	before :each do
+		@shop = HarryPotterBookShop.new
+	end
+
 	it 'does not charge for no books' do
-		shop = HarryPotterBookShop.new
-		shop.buy(0).should == 0
+		@shop.buy(0).should == 0
 	end
 
 	it 'gives no discount for a single book' do
-		shop = HarryPotterBookShop.new
-		shop.buy(1).should == 42
+		@shop.buy(1).should == 42
 	end
 
 	it 'gives 5% of discount for two books' do
-		shop = HarryPotterBookShop.new
-		shop.buy(2).should == (2*42)*(1 - 0.05)
+		@shop.buy(2).should == (2*42)*(1 - 0.05)
 	end
 
 	it 'gives 10% of discount for three books' do
-		shop = HarryPotterBookShop.new
-		shop.buy(3).should == (3*42)*(1 - 0.1)
+		@shop.buy(3).should == (3*42)*(1 - 0.1)
 	end
 
 	it 'gives 15% of discount for four books' do
-		shop = HarryPotterBookShop.new
-		shop.buy(4).should == (4*42)*(1 - 0.15)
+		@shop.buy(4).should == (4*42)*(1 - 0.15)
+	end
+
+	it 'gives 20% of discount for five books' do
+		@shop.buy(5).should == (5*42)*(1 - 0.2)
 	end
 end
